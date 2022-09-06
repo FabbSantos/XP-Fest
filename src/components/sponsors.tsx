84f1f16e90dc9666a81bda9c913c82b0f8cebf95
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2022-09-05 23:40:05 -0300

    some changes in some imports

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,19 +1,23 @@
 import { motion } from 'framer-motion'
 import Slide from './slide/slide';
 
+import sponsors from '../assets/patrocinadores.gif'
+import clouds_inverted from '../assets/clouds_inverted.png'
+import clouds_mirror_inverted from '../assets/clouds_mirror_inverted.png'
+
 function Sponsors() {
 
     return (
         <>
             <div className="max-w-screen lg:min-h-screen py-20 lg:py-0 flex flex-col relative items-center  justify-center">
-                <motion.img src="./src/assets/patrocinadores.gif" alt='Conheça os patrocinadores'
+                <motion.img src={sponsors} alt='Conheça os patrocinadores'
                     initial={{ opacity: 0, x:'-5rem' }}
                     whileInView={{ opacity: 1, x: 0 }}
                     transition={{ delay: 0.5, duration: 1.5 }} 
                 /> 
 
-                <img src="./src/assets/clouds_inverted.png" alt="clouds" className="absolute top-0 left-0  hidden lg:block max-h-[476px] lg:max-h-[400px] w-auto" />
-                <img src="./src/assets/clouds_mirror_inverted.png" alt="clouds" className="absolute top-0 right-0 hidden lg:block lg:max-h-[400px] w-auto" />
+                <img src={clouds_inverted} alt="clouds" className="absolute top-0 left-0  hidden lg:block max-h-[476px] lg:max-h-[400px] w-auto" />
+                <img src={clouds_mirror_inverted} alt="clouds" className="absolute top-0 right-0 hidden lg:block lg:max-h-[400px] w-auto" />
 
                 <motion.h4 className='robot text-center text-lg lg:max-w-[1100px] lg:mt-[-10rem] lg:mb-[4rem]'
                     initial={{ opacity: 0, x:'-5rem' }}
